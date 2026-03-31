8334970493b7b123e8156f0ef6995a02211ada46
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-03-30 19:30:22 -0700

    implemented full CRUD for frontend ui and setup authentication middleware with passport, sessions and cookie-parser

--- src/app.js
+++ src/app.js
@@ -4,13 +4,39 @@ import express from 'express';
 import indexRouter from './routers/indexRoutes.js';
 import gamesRouter from './routers/gamesRoutes.js';
 
+import session from 'express-session';
+import passport from 'passport';
+import cookieParser from 'cookie-parser';
+
+
 // create instance of express app
 const app = express();
 
 // middleware to parse JSON request bodies
 app.use(express.json());
 
+// Makes files in /public accessible via browser (e.g., /styles.css, /script.js)
 app.use(express.static('public'));
+
+// ===================
+// COOKIE PARSER
+// ===================
+app.use(cookieParser());
+
+// ===================
+// SESSION SETUP
+// ===================
+app.use(session({
+  secret: 'secretkey', // change later
+  resave: false,
+  saveUninitialized: false
+}));
+
+// ====================
+// PASSPORT SETUP
+// ====================
+app.use(passport.initialize());
+app.use(passport.session());
 
 // set EJS as view engine
 app.set('view engine', 'ejs');
